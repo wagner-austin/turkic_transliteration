11c8a450699b8bb490067b9ea62bbe81a1a6a3c7
wagner-austin <austinwagner@msn.com>
2025-05-21 05:54:31 -0700

    Add fastText model documentation and attribution

--- src/turkic_translit/model_utils.py
+++ src/turkic_translit/model_utils.py
@@ -1,5 +1,41 @@
 """
 Utilities for downloading and managing model files required by the Turkic Transliteration Suite.
+
+FastText Language Identification Model Information:
+-------------------------------------------------
+We use two models for language identification, which can recognize 176 languages:
+- lid.176.bin: faster and slightly more accurate, file size of 126MB
+- lid.176.ftz: compressed version of the model, file size of 917kB
+
+These models were trained on data from Wikipedia, Tatoeba and SETimes, used under CC-BY-SA.
+The models expect UTF-8 as input.
+
+License: Creative Commons Attribution-Share-Alike License 3.0.
+
+Supported languages (ISO codes):
+af als am an ar arz as ast av az azb ba bar bcl be bg bh bn bo bpy br bs bxr ca cbk ce ceb ckb co cs cv cy da de
+diq dsb dty dv el eml en eo es et eu fa fi fr frr fy ga gd gl gn gom gu gv he hi hif hr hsb ht hu hy ia id ie
+ilo io is it ja jbo jv ka kk km kn ko krc ku kv kw ky la lb lez li lmo lo lrc lt lv mai mg mhr min mk ml mn mr
+mrj ms mt mwl my myv mzn nah nap nds ne new nl nn no oc or os pa pam pfl pl pms pnb ps pt qu rm ro ru rue sa sah
+sc scn sco sd sh si sk sl so sq sr su sv sw ta te tg th tk tl tr tt tyv ug uk ur uz vec vep vi vls vo wa war wuu
+xal xmf yi yo yue zh
+
+References:
+[1] A. Joulin, E. Grave, P. Bojanowski, T. Mikolov, Bag of Tricks for Efficient Text Classification
+    @article{joulin2016bag,
+      title={Bag of Tricks for Efficient Text Classification},
+      author={Joulin, Armand and Grave, Edouard and Bojanowski, Piotr and Mikolov, Tomas},
+      journal={arXiv preprint arXiv:1607.01759},
+      year={2016}
+    }
+
+[2] A. Joulin, E. Grave, P. Bojanowski, M. Douze, H. Jégou, T. Mikolov, FastText.zip: Compressing text classification models
+    @article{joulin2016fasttext,
+      title={FastText.zip: Compressing text classification models},
+      author={Joulin, Armand and Grave, Edouard and Bojanowski, Piotr and Douze, Matthijs and J{\'e}gou, H{\'e}rve and Mikolov, Tomas},
+      journal={arXiv preprint arXiv:1612.03651},
+      year={2016}
+    }
 """
 
 import logging
